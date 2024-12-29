52646803e9cdf1456721ae53be362811cea53e87
N S Pious <nspious1999@gmail.com>
2024-12-29 17:19:28 +0530

    apinewchangess15

--- apiGateway/server.ts
+++ apiGateway/server.ts
@@ -28,7 +28,7 @@ app.use(
     "/user-service",
     (req, res, next) => {
       // Apply authorization middleware only to private routes
-      const publicRoutes = ["/login", "/register" ,"/adminlogin"];
+      const publicRoutes = ["/login", "/register" ,"/adminlogin" ,"/getbanneruser"];
       if (publicRoutes.some((route) => req.path.startsWith(route))) {
         console.log("Public route accessed in user-service:", req.path);
         return next(); // Skip authorization
